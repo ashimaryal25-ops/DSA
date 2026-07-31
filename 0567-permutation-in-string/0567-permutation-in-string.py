995ef4a863d4321286b014fada3c8397fbe1f485
Ashim Aryal <ashimaryal25@gmail.com>
2026-07-30 20:22:17 -0400

    Time: 19 ms (90.1%), Space: 12.8 MB (46.24%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,56 +1,57 @@
 class Solution(object):
     def checkInclusion(self, s1, s2):
         
-        map1 = {}
-        for s in s1:
-            if s in map1:
-                map1[s] += 1
-            else:
-                map1[s] = 1    
-
-
-        if len(s2) < len(s1):
+        if len(s1) > len(s2):
             return False
+
+        if not s1:
+            return True
+
+        if not s2:
+            return False
+
+        char_map = collections.defaultdict(int)
+        window_map = collections.defaultdict(int)
+
+        # making the map
+
+        for c in s1:
+
+            char_map[c] += 1
+
+
+        #initial dict for initial window of s2:
+        
+        for i in range(len(s1)):
+
+            window_map[s2[i]] += 1
+            
+        
+        if char_map == window_map:
+            return True
+
+
         left = 0
         right = len(s1)
 
-        windowMap = {}
-
-        #mapping initial window
-
-        for i in range( right ):
-
-            if s2[i] in windowMap:
-                windowMap[s2[i]] += 1
-
-            else:
-                windowMap[s2[i]] = 1    
-
-        if  windowMap == map1:
-            return True
-
-        for i in range( right, len(s2) ):
-
-            windowMap[s2[left]] -= 1
-
-            if windowMap[s2[left]] == 0:
-                windowMap.pop(s2[left])
-
-           
-
-            if s2[right] in windowMap:
-                windowMap[s2[right]] += 1
-            else:    
-                windowMap[s2[right]] = 1
-
-            left += 1
-
-            right += 1
-
-            if windowMap == map1:
-                return True    
         
+        while right < len(s2) :
+
+            window_map[s2[left]] -= 1
+
+            if window_map[s2[left]] == 0:
+                del window_map[s2[left]]
+
+            window_map[s2[right]] += 1    
+
+            if char_map == window_map:
+                return True
+            
+            left = left + 1
+
+            right = right + 1
+
+
         return False
 
-            
-            
+        
