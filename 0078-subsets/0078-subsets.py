5bc7606a47ebcd2b97f89214f049459ad65a2ef9
Ashim Aryal <ashimaryal25@gmail.com>
2026-08-01 20:41:54 -0400

    Time: 0 ms (100%), Space: 12.8 MB (23.07%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,13 +1,16 @@
 class Solution(object):
     def subsets(self, nums):
-        res = [[]]
-        def dfs(start_index, current_form):
+        res = []
+        stack = []
+
+        def dfs(start_index):
+            res.append(list(stack))
 
             for i in range(start_index, len(nums)):
+                stack.append(nums[i])
+                dfs(i + 1)
+                stack.pop()
 
-                current_form.append( nums[i] )
-                res.append(list(current_form))
-                dfs( i+1, current_form)
-                current_form.pop()
-        dfs(0, []) 
-        return res       
+        dfs(0)
+        return res
+    
